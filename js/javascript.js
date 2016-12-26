25d24d8db6aad87fcc77ca9ee3338ae032330622
Tommy Huynh <TommysMAC@Tommys-MBP.home>
2016-12-25 17:14:19 -0800

    asdf

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+
+
+
+
+
+
+}); 
